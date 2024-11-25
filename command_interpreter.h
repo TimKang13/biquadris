4b20cd247e655211efe0406f0cb8d61f8f2a1ff5
Jeffrey Zhao <jeffzhao2015@gmail.com>
2024-11-25 15:09:13 -0500

    completed command interpreter class for now, needs to finish macros later

--- command_interpreter.h
+++ command_interpreter.h
@@ -8,10 +8,8 @@ class CommandInterpreter {
     std::map<std::string, std::string> commands;
     std::vector<std::string> macros; // to be implemented later, not sure how macro works yet
     std::pair<int, std::string> commandToReturn;
-    std::string fileName;
-    std::pair<int, std::string> parseCommand(const std::string &input);
     std::pair<int, std::string> separateNumber(std::string word);
-    bool isValid(std::string word);
+    bool isValid();
     void updateNum();
     std::vector<std::string> getCommandValue(std::string temp);
     void rename(std::string oldName, std::string newName);
